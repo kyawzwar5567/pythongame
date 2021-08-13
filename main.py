2372ade18b66f75eeb425fcb5038bd62f5618784
kyawzwarpythongame <kyawzwar5567@gmail.com>
2021-08-13 22:06:03 +0630

    adding new lines

--- main.py
+++ main.py
@@ -72,11 +72,14 @@ wn.onkeypress(paddle_b_down, "Down")
 while True:
     wn.update()
    #move the ball
+
+
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
 
     #Border checking
+
     if ball.ycor() > 290 :
         ball.sety(290)
         ball.dy *= -1
@@ -84,3 +87,7 @@ while True:
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0)
+        ball.dx *= -1
